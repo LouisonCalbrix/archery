3532c0fe35141b967fcf57647c5aaf143e8ec7b1
Louison Calbrix <34002086+LouisonCalbrix@users.noreply.github.com>
2019-12-01 19:43:33 +0100

    Added sprite and speed to Bow class A Bow object now has an _img, a _rect and a _speed attributes which together make it possible to display an image onscreen and move it across the screen. The bow object aattributes are updated via its update method.

--- archery.py
+++ archery.py
@@ -8,12 +8,16 @@
 import os
 import pygame
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+
 class Bow(pygame.sprite.Sprite):
     '''
     A bow moving across the screen from top to bottom and back.
     It can shoot arrows.
     '''
-    IMG = pygame.image.load('resources/bow.png')
+    IMG = pygame.image.load('resources/bow1.png')
+    SPEED = [0, 5]
 
     def __init__(self):
         '''
@@ -21,9 +25,41 @@ class Bow(pygame.sprite.Sprite):
         '''
         super().__init__()
         self._img = Bow.IMG
+        self._rect = self._img.get_rect()
+        self._rect.move_ip(20, 0)
+        self._speed = Bow.SPEED.copy()
+
+    def update(self):
+        '''
+        Update bow's position, and whether or not an arrow is shot.
+        '''
+        self._rect.move_ip(*self._speed)
+        if self._rect.y < 0 or self._rect.y > SCREEN_HEIGHT-self._rect.height:
+            self._speed = [-speed_component for speed_component in self._speed]
+
 
 class Arrow:
     pass
 
 class Target:
     pass
+
+# for test purpose
+if __name__ == '__main__':
+    
+    # pygame init
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    fps = 40
+    bow = Bow()
+    while True:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        bow.update()
+        screen.blit(bow._img, bow._rect)
+        pygame.display.flip()
+        clock.tick(fps)
